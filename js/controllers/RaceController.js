0efdb4ff6322cd0e0d901b544c112e069e6d3821
Subhrajit Debnath <sdebnath@jahnelgroup.com>
2016-05-24 16:55:43 -0400

    Added functionality to get input from particular race

--- js/controllers/RaceController.js
+++ js/controllers/RaceController.js
@@ -4,6 +4,13 @@ angular
 .controller("RaceController", ["$scope", "$routeParams", "RaceToHomeService", function ($scope, $routeParams, RaceToHomeService) {
 	//Sets the current race number
 	$scope.raceNum = Number($routeParams.raceId) + 1;
+	$scope.startingTickets = [];
+	$scope.totalMoney;
+	$scope.horses = [];
+
+	for (i = 0; i < 6; i++){
+		$scope.horses[i] = {horseNum: i+1};
+	}
 
 	//Uses the RaceToHomeService and RaceToHistService to save relevent
 	$scope.submitRace = function () {

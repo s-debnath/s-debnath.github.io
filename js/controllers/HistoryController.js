d4aae5c13bcde6aeaf72b58474088cf6c7e2f62e
Subhrajit Debnath <sdebnath@jahnelgroup.com>
2016-05-25 16:54:16 -0400

    Added history page and contoller information

--- js/controllers/HistoryController.js
+++ js/controllers/HistoryController.js
@@ -1,6 +1,11 @@
 // Controller for race view
 angular
 .module("RaceApp")
-.controller("HistoryController", ["$scope", function ($scope) {
+.controller("HistoryController", ["$scope", "RaceServices", function ($scope, RaceServices) {
+	//Uses the RaceServices to check if races have started
+	$scope.curRaceNum = RaceServices.getRaceNum();
+	$scope.racesStarted = ($scope.curRaceNum > 0);
+	$scope.races = [{}];
+	$scope.races = RaceServices.getRaces();
 
 }]);

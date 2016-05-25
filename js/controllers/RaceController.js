a49a2654e63506cb11545b27f681773e65befdc0
Subhrajit Debnath <sdebnath@jahnelgroup.com>
2016-05-25 10:43:03 -0400

    Added more css

--- js/controllers/RaceController.js
+++ js/controllers/RaceController.js
@@ -3,9 +3,7 @@ angular
 .module("RaceApp")
 .controller("RaceController", ["$scope", "$routeParams", "RaceToHomeService", function ($scope, $routeParams, RaceToHomeService) {
 	//Sets the current race number
-	$scope.raceNum = Number($routeParams.raceId) + 1;
-	$scope.startingTickets = [];
-	$scope.totalMoney;
+	$scope.raceNum = $routeParams.raceId + 1;
 	$scope.horses = [];
 
 	for (i = 0; i < 6; i++){
